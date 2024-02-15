a1c4402c85931dec9ba937276724c44d54d7512d
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-15 09:29:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function makeid(l) {
   // write your code here
 	let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-
 	let result = "";
 	
 	for(let loop=0; loop<l; loop++){
-		result += characters.charAt(Math.floor(Math.rand(0,62));
+		result += characters.charAt(Math.floor(Math.random()*62));
 	}
 
 	return result;
